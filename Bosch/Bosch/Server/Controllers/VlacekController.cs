a9ebc32510a49230a605f00e690d439a0b310282
David Zeman <david.zeman@outlook.com>
2022-04-11 15:11:36 +0200

    Vlacky

---
+++ Bosch/Bosch/Server/Controllers/VlacekController.cs
@@ -0,0 +1,25 @@
+﻿using Bosch.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bosch.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VlacekController : ControllerBase
+    {
+        public static Vlacek[] Seznam = new Vlacek[]
+            {
+                new Vlacek() { Id = 0, Typ = "Nakladni", Material = "Uhli", Kapacita = 20 },
+                new Vlacek() { Id = 1, Typ = "Nakladni", Material = "Drevo", Kapacita = 50 },
+                new Vlacek() { Id = 2, Typ = "Osobni", Material = "Lidi", Kapacita = 100 },
+            };
+
+        [HttpGet]
+        [Route("/api/vlacek/list")]
+        public Vlacek[] List()
+        {
+            return Seznam;
+        }
+    }
+}

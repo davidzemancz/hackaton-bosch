9c2cb8f1d2bca69556bb7b5bdf9174a9e7cd3fe0
kwimbik <kwimbiks@gmail.com>
2022-04-13 11:46:45 +0200

    pridani material component

---
+++ Bosch/Bosch/Server/Controllers/MaterialController.cs
@@ -0,0 +1,26 @@
+﻿using Bosch.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Bosch.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/api/vlacek/edit/{id:int}")]
+        public void Find(int Id)
+        {
+            //TODO
+        }
+
+        [HttpGet]
+        [Route("/api/vlacek/list")]
+        public Vlacek[] List()
+        {
+            return Database.Vlacky.ToArray();
+        }
+    }
+}

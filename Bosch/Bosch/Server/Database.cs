226411ae73b063aba52e76090ed93fa4d66ea402
David Zeman <david.zeman@outlook.com>
2022-04-13 11:47:24 +0200

    Merge database s luckou

--- Bosch/Bosch/Server/Database.cs
+++ Bosch/Bosch/Server/Database.cs
@@ -13,19 +13,33 @@ namespace Bosch.Server
             new Vlacek() { Id = 2,  Kapacita = 100 },
         };
 
+        public static List<Material> Materialy { get; set; } = new List<Material>();
+
+        public static List<Zastavka> Zastavky { get; set; } = new List<Zastavka>()
+        {
+            new Zastavka() {
+                Mezisklad = Mezisklady[0], Cas = new DateTime(2008, 5, 1, 8, 30, 52) },
+            new Zastavka() {
+                Mezisklad = Mezisklady[1], Cas = new DateTime(2008, 5, 1, 8, 30, 52) },
+            new Zastavka() {
+                Mezisklad = Mezisklady[2], Cas = new DateTime(2008, 5, 1, 8, 30, 52) },
+            new Zastavka() {
+                Mezisklad = Mezisklady[3], Cas = new DateTime(2008, 5, 1, 8, 30, 52) },
+            new Zastavka() {
+                Mezisklad = Mezisklady[4], Cas = new DateTime(2008, 5, 1, 8, 30, 52) },
+            new Zastavka() {
+                Mezisklad = Mezisklady[5], Cas = new DateTime(2008, 5, 1, 8, 30, 52) },
+
+        };
+
         public static List<Trasa> Trasy = new List<Trasa>
             {
-                new Trasa() { Id = 0, Zastavky = new List<Zastavka> {
-                    new Zastavka() { Mezisklad = null, Cas = new DateTime(2008, 5, 1, 8, 30, 52) } } },
+                new Trasa() { Id = 0, Zastavky = Zastavky },
 
-                new Trasa() { Id = 1, Zastavky = new List<Zastavka> {
-                    new Zastavka() { Mezisklad = null, Cas = new DateTime(2020, 8, 1, 8, 30, 52) } } },
+                new Trasa() { Id = 1, Zastavky = Zastavky },
 
-                new Trasa() { Id = 2, Zastavky = new List<Zastavka> {
-                    new Zastavka() { Mezisklad = null, Cas = new DateTime(2022, 5, 10, 8, 30, 52) } } },
+                new Trasa() { Id = 2, Zastavky = Zastavky }
             };
-
-        public static List<Material> Materialy { get; set; } = new List<Material>();
 
         public static List<JizdniRad> JizdniRady { get; set; } = new List<JizdniRad>
         {
@@ -37,6 +51,15 @@ namespace Bosch.Server
             },
         };
 
-        public static List<Mezisklad> Mezisklady { get; set; } = new List<Mezisklad>();
+        public static List<Mezisklad> Mezisklady { get; set; } = new List<Mezisklad>()
+        {
+            new Mezisklad { Id = 0, Jmeno = "Praha", jizdniRad = null, seznamVlacku = { } },
+            new Mezisklad { Id = 1, Jmeno = "Brno", jizdniRad = null, seznamVlacku = { } },
+            new Mezisklad { Id = 2, Jmeno = "Ostrava", jizdniRad = null, seznamVlacku = { } },
+            new Mezisklad { Id = 3, Jmeno = "Liberec", jizdniRad = null, seznamVlacku = { } },
+            new Mezisklad { Id = 4, Jmeno = "Olomouc", jizdniRad = null, seznamVlacku = { } },
+            new Mezisklad { Id = 5, Jmeno = "Pardubice", jizdniRad = null, seznamVlacku = { } },
+        };
+
     }
 }
